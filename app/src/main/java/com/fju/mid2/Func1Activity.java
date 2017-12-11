cf54abe55931189d9d61891d72c2f39b02793990
psli0101 <p.s.li19980101@gmail.com>
2017-12-11 13:40:23 +0800

    homework1

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,13 +1,27 @@
 package com.fju.mid2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
+
+    public int num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
+    }
+
+    public void roll(View view) {
+        Random random = new Random();
+        this.num = random.nextInt(6) + 1;
+        new AlertDialog.Builder(this)
+                .setMessage( num + "")
+                .show();
     }
 }
